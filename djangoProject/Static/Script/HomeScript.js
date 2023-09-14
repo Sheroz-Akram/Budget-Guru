d41202562b66ee610e5aedacde4bd53a6fdeb8eb
Sheroz Akram <sheroz.akram@proton.me>
2023-09-14 19:45:53 +0500

    Response Menu
    Created a responsive menu of nav bar for mobile screen. Also add menu button on the top right corner.

--- djangoProject/Static/Script/HomeScript.js
+++ djangoProject/Static/Script/HomeScript.js
@@ -1,4 +1,19 @@
-// Toggle Menu For Navigation Bar
-function toggleMenu(){
+// Toggle Menu For Navigation Bar Mobile Menu
+let navBarMobile = 0;
+function toggleMenu() {
+
+    // get Nav Bar Menu For Mobile
+    let mobileMenu = document.getElementById("navBarMobileMenu");
+
+    // Toggle On
+    if (navBarMobile == 1) {
+        mobileMenu.style.display = "none";
+        navBarMobile = 0;
+    }
+    // Toggle Off
+    else {
+        mobileMenu.style.display = "block";
+        navBarMobile = 1;
+    }
 
 }
