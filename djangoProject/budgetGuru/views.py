a36371dd0fd6f7066f2043f3482606c14d01f347
Sheroz Akram <sheroz.akram@outlook.com>
2024-04-28 15:29:34 +0500

    implement login functionality

--- djangoProject/budgetGuru/views.py
+++ djangoProject/budgetGuru/views.py
@@ -61,7 +61,58 @@ def registrationAPI(request):
 
 # Login API (login a already registored user from the database)
 def loginAPI(request):
-    return HttpResponse("Got a Request. Okay")
+
+    # Check if User is already Login or Not
+    if request.session.get('loginStatus', False) == True:
+        return HttpResponseRedirect('/Dashboard')
+
+    try:
+        # Get the Information of the User
+        login_email_address = request.POST['loginEmail']
+        login_password = request.POST['loginPassword']
+
+        try:
+
+            # Create a unique private key
+            privatekey = generateRandomeString(100)
+
+            # Check if the user already exists or not
+            user = AppUser.objects.get(
+                email_address=login_email_address,
+            )
+
+            # Check if Entered Password is correct or not
+            if user.password != login_password:
+                return render(request=request, template_name="Account.html", context={
+                    "status": "error",
+                    "message":"Entered password is not correct."
+                })
+
+            # Store the Private Key to the User Account
+            user.private_key = privatekey
+            user.save()
+
+            # Store the User Data into New Session
+            request.session['loginStatus'] = True
+            request.session['emailAddress'] = login_email_address
+            request.session['privateKey'] = privatekey
+
+            # Return the User to the Dashboard Page
+            return HttpResponseRedirect('/Dashboard')
+
+        # User of the application does not found
+        except Exception as e:
+            return render(request=request, template_name="Account.html", context={
+                "status": "error",
+                "message":"User does not found. Please enter correct details."
+            })
+
+    # An Error in the Request
+    except Exception as e:
+        return render(request=request, template_name="Account.html", context={
+                "status": "error",
+                "message":"An invalid request by the user"
+            })
 
 # Dashboard Page that is Shown to the User
 def DashboardPage(request):
